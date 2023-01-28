b93a0c1053eca7cdd881042635082e63361a1929
Everton Silva <everton.ss7@icloud.com>
2023-01-28 14:22:37 -0300

    Projeto de Habitos com Html, css e js

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', () => {
+   
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {        
+        return
+    }
+
+    nlwSetup.addDay(today)
+})
+
+form.addEventListener("change", () => {
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))        
+})
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
